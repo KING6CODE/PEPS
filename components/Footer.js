10da3297d07f31eaaeb6267d18164149b52eb1ff
KING6CODE <alistpassecoutrin@gmail.com>
2025-08-27 10:50:38 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+```js
+export default function Footer() {
+return (
+<footer className="border-t mt-12">
+<div className="container py-8 flex flex-col md:flex-row items-center justify-between">
+<div className="font-bold text-lg">PEPS</div>
+<div className="text-sm text-gray-600">Mentions Usg|es</div>
+</div>
+</footer>
+)
+}
+```
