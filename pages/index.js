a5a48354a7c31eef77b8e64f7e24141f09bb53ac
KING6CODE <alistpassecoutrin@gmail.com>
2025-08-27 10:51:12 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+```js
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Services from '../components/Services'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+
+export default function Home() {
+return (
+<div>
+<Header />
+<main>
+<Hero />
+<About />
+<Services />
+<Contact />
+</main>
+<Footer />
+</div>
+)
+}
+```
